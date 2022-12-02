dff89dd326d223996572a322a1ac8cf6a264a68d
Johannes Neumeier <hello@johannesneumeier.com>
2022-12-02 16:28:59 +0200

    PoC for pdf

--- make-pdf.sh
+++ make-pdf.sh
@@ -18,4 +18,10 @@ echo "wkhtmltopdf --print-media-type $URL/ $DIR/index.pdf"
 wkhtmltopdf --print-media-type $URL/ $DIR/index.pdf
 wkhtmltopdf --print-media-type $URL/1-what-do-we-mean-by-legibility $DIR/1.pdf
 wkhtmltopdf --print-media-type $URL/2-how-we-read $DIR/2.pdf
-gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=$DIR/book.pdf $DIR/index.pdf $DIR/1.pdf $DIR/2.pdf
+wkhtmltopdf --print-media-type $URL/3-perspectives-on-legibility $DIR/3.pdf
+wkhtmltopdf --print-media-type $URL/4-what-is-measured-and-how $DIR/4.pdf
+wkhtmltopdf --print-media-type $URL/5-overview-of-research $DIR/5.pdf
+wkhtmltopdf --print-media-type $URL/6-beyond-legibility-research $DIR/6.pdf
+wkhtmltopdf --print-media-type $URL/bibliography $DIR/bibliography.pdf
+gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=$DIR/book.pdf $DIR/index.pdf $DIR/1.pdf $DIR/2.pdf $DIR/3.pdf $DIR/4.pdf $DIR/5.pdf $DIR/6.pdf $DIR/bibliography.pdf
+cp $DIR/book.pdf assets/downloads/legibility.pdf
